09ffc726a587cc62fda0185918868acd3d53f0a5
leyohen <35643042+leyohen@users.noreply.github.com>
2019-11-18 12:11:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const video = document.querySelector("#video")
+
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+	faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+	faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+	faceapi.nets.faceExpressionNet.loadFromUri('models')
+	]).then(startVideo)
+
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
+
+
+
+
+
+
+
+
+
+//https://www.kirupa.com/html5/accessing_your_webcam_in_html5.htm
+
+/*
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
+
+*/
